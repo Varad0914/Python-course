d98323e89205163035ec25478a4e80df78726bcd
Varad0914 <varadshinde1409@gmail.com>
2026-09-09 20:49:10 +0530

    first commit

---
+++ Basic.py
@@ -0,0 +1,28 @@
+
+# for i in range(4):
+#     for j in range(4):
+#      print("#" ,end="")
+#     print()
+
+
+# import numpy as np
+
+# arr = np.array([10,20,30,40,50])
+# print(arr)
+
+# import pandas as pd 
+
+# data ={
+#     "Name" :["varad","raj"],
+#     "Age" :[18,19],
+#     "Marks" :[66, 78]
+# }
+# df = pd.DataFrame(data)
+# print(df.head(1))
+
+import matplotlib.pyplot as pt
+
+x = [1,2,3,34]
+y = [22,31,3,44]
+pt.plot(x ,y)
+pt.show()
